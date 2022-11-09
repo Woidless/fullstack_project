bd088a472322bd376ff1b3519f0f414f3ddb5f67
Woidless <bmoblp@mail.ru>
2022-11-09 17:45:11 +0600

    fix

--- account/send_email.py
+++ account/send_email.py
@@ -2,7 +2,7 @@ from django.core.mail import send_mail
 
 
 def send_confirmation_email(user, code):
-    full_link = f'http://34.28.220.66/accounts/activate/{code}/'
+    full_link = f'http://34.133.205.247/accounts/activate/{code}/'
     send_mail(
         'Hello! Activate your account!',
         f'To activate your account you need to link: {full_link}',
